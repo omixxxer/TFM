933ef41afd2e31bfe3cf2f5e868d69ff370d291a
omixxxer <147170109+omixxxer@users.noreply.github.com>
2024-10-31 11:56:05 +0100

    Update tracking_node.py

--- person_follower/tracking_node/tracking_node.py
+++ person_follower/tracking_node/tracking_node.py
@@ -10,20 +10,24 @@ import numpy as np
 class TrackingNode(Node):
     def __init__(self):
         super().__init__('tracking_node')
-        self.cmd_vel_publisher = self.create_publisher(Twist, '/cmd_vel', 10)
+        self.cmd_vel_subscription = self.create_subscription(Twist, '/cmd_vel', self.cmd_vel_callback, 10)
         self.person_detected_subscription = self.create_subscription(Bool, '/person_detected', self.detection_callback, 10)
         self.scan_subscription = self.create_subscription(LaserScan, '/scan', self.listener_callback, 10)
         
+        # Variables de estado
         self.min_distance = 0.4
         self.prev_angles = []
         self.angle_filter_window = 5
         self.person_detected = False
+        self.latest_cmd_vel = Twist()  # Últimos comandos de movimiento recibidos del nodo de control
         self.get_logger().info("Nodo de Seguimiento iniciado")
 
     def detection_callback(self, msg):
+        """Callback para actualizar el estado de detección de personas"""
         self.person_detected = msg.data
 
     def listener_callback(self, input_msg):
+        """Callback de LIDAR para calcular el ángulo hacia la persona detectada"""
         if not self.person_detected:
             return
 
@@ -34,13 +38,13 @@ class TrackingNode(Node):
         if self.prev_angles:
             angle_to_person = self.smooth_angle(angle_to_person - angle_min)
 
-        vx = 0.05 if min(ranges) < self.min_distance else 0.35
-        wz = 2.0 * -angle_to_person if abs(angle_to_person) > 0.6 else -0.6
+        self.latest_cmd_vel.angular.z = 2.0 * -angle_to_person if abs(angle_to_person) > 0.6 else -0.6
 
-        output_msg = Twist()
-        output_msg.linear.x = vx
-        output_msg.angular.z = wz
-        self.cmd_vel_publisher.publish(output_msg)
+    def cmd_vel_callback(self, msg):
+        """Callback que recibe comandos de velocidad desde el Nodo de Control y los publica al robot"""
+        self.latest_cmd_vel.linear.x = msg.linear.x
+        self.latest_cmd_vel.angular.z = msg.angular.z
+        self.get_logger().info(f"Aplicando comando de control: linear.x = {msg.linear.x}, angular.z = {msg.angular.z}")
 
     def smooth_angle(self, angle):
         angles_rad = [math.radians(a) for a in self.prev_angles]
